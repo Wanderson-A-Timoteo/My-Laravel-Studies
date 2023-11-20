eceba7806e58b3334d59debe7b2bf76f34f97571
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-11-20 12:31:26 -0400

    add create: forma convencional de criar um registro no banco.

--- crud/app/Http/Controllers/PostController.php
+++ crud/app/Http/Controllers/PostController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $new_post = [
+            'title' => 'Meu primeiro post',
+            'content' => 'Conteúdo qualquer',
+            'author' => 'Wanderson'
+        ];
+
+        $posts = new Post($new_post);
+        $posts->save();
+
+        dd($posts);
+    }
 }
